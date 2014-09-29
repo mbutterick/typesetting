84a82606d0d9ebda5926af14244c47b63c0dcf48
Matthew Butterick <mb@mbtype.com>
2014-09-29 08:30:26 -0700

    count_if

--- csp/utils.rkt
+++ csp/utils.rkt
@@ -1,3 +1,12 @@
 #lang racket/base
+(require racket/list racket/bool)
 
 (provide (all-defined-out))
+
+(module+ test (require rackunit))
+
+(define (count_if pred xs)
+  (length (filter-not false? (map pred xs))))
+
+(module+ test
+  (check-equal? (count_if procedure? (list 42 null max min)) 2))
