70ca48255bf560c6d49fc74ed40cff8f861c1373
Matthew Butterick <mb@mbtype.com>
2014-10-06 11:26:09 -0700

    paste problem

--- csp/test-einstein.rkt
+++ csp/test-einstein.rkt
@@ -2,6 +2,35 @@
 
 (require "problem.rkt" "constraint.rkt" sugar/debug)
 
+#|
+# There are no tricks, just pure logic, so good luck and don't give up. 
+# 
+# 1. In a street there are five houses, painted five different colours. 
+# 2. In each house lives a person of different nationality 
+# 3. These five homeowners each drink a different kind of beverage, smoke 
+# different brand of cigar and keep a different pet. 
+# 
+# THE QUESTION: WHO OWNS THE zebra? 
+# 
+# HINTS 
+# 
+# 1. The englishman lives in a red house. 
+# 2. The spaniard keeps dogs as pets. 
+# 5. The owner of the Green house drinks coffee. 
+# 3. The ukrainian drinks tea. 
+# 4. The Green house is on the left of the ivory house. 
+# 6. The person who smokes oldgold rears snails. 
+# 7. The owner of the Yellow house smokes kools. 
+# 8. The man living in the centre house drinks milk. 
+# 9. The Norwegian lives in the first house. 
+# 10. The man who smokes chesterfields lives next to the one who keeps foxes. 
+# 11. The man who keeps horses lives next to the man who smokes kools. 
+# 12. The man who smokes luckystrike drinks orangejuice. 
+# 13. The japanese smokes parliaments. 
+# 14. The Norwegian lives next to the blue house. 
+# 15. The man who smokes chesterfields has a neighbour who drinks water. 
+|#
+
 (define ep (new problem%))
 
 (for ([idx '(1 2 3 4 5)])
