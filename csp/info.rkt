5cecfa2a6941ac8099bb1d15b246468e5f1f430b
Matthew Butterick <mb@mbtype.com>
2014-10-13 11:43:37 -0700

    add info.rkt

---
+++ csp/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "csp")
+(define deps '(("base" #:version "6.0") "sugar"))
+(define update-implies '("sugar"))
+;(define scribblings '(("scribblings/pollen.scrbl" (multi-page))))
+;(define raco-commands '(("pollen" pollen/raco "issue Pollen command" #f)))
+;(define compile-omit-paths '("tests" "raco.rkt"))
