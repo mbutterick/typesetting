b5de86f326f6dcbd89b40be63c7c6d7dde00455d
Matthew Butterick <mb@mbtype.com>
2014-10-28 15:58:00 -0700

    correct dependency

--- csp/info.rkt
+++ csp/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "csp")
-(define deps '(("base" #:version "6.0") "sugar"))
+(define deps '(("base" #:version "6.0") "sugar" "rackunit-lib"))
 (define update-implies '("sugar"))
 ;(define scribblings '(("scribblings/pollen.scrbl" (multi-page))))
 ;(define raco-commands '(("pollen" pollen/raco "issue Pollen command" #f)))
