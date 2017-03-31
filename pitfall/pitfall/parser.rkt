acddf1dba10f7a74eef155655c57b5814f18433d
Matthew Butterick <mb@mbtype.com>
2017-03-31 08:51:27 -0700

    grammar

--- pitfall/pitfall/parser.rkt
+++ pitfall/pitfall/parser.rkt
@@ -1,7 +1,8 @@
 #lang brag
 
-pf-program : pf-object*
-@pf-object : pf-null | CHAR | BOOLEAN | INT | REAL | pf-name | pf-string | pf-array | pf-dict | pf-stream | pf-indirect-object | pf-indirect-object-ref | pf-header | pf-comment
+pf-program : pf-header pf-object*
+pf-header : PDF-HEADER
+@pf-object : pf-null | CHAR | BOOLEAN | INT | REAL | pf-name | pf-string | pf-array | pf-dict | pf-stream | pf-indirect-object | pf-indirect-object-ref | pf-comment
 @pf-null : NULL
 pf-name : NAME
 pf-string : STRING-TOK | /"<" HEX-DIGIT-PAIR* /">"
@@ -12,5 +13,4 @@ pf-dict : /"<" /"<" (pf-dict-key pf-dict-value)* /">" /">"
 pf-stream : pf-dict STREAM-DATA
 pf-indirect-object : INT INT /"obj" pf-object /"endobj"
 pf-indirect-object-ref : INDIRECT-OBJECT-REF-TOK
-pf-header : PDF-HEADER
 pf-comment : COMMENT
