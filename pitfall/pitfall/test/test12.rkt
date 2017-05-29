7189dae52e42976a136d47f8505c468255c66db5
Matthew Butterick <mb@mbtype.com>
2017-05-28 17:21:10 -1000

    start test12

---
+++ pitfall/pitfall/test/test12.rkt
@@ -0,0 +1,15 @@
+#lang pitfall/pdftest
+
+(define (proc doc)
+  (send* doc
+    [fillColor "blue"]
+    [font "Helvetica" 30]
+    [text "Here is a link!" 100 100 (hash
+                                     'link "http://google.com/"
+                                     'underline #t)]))
+
+(define-runtime-path this "test11rkt.pdf")
+(make-doc this #f proc #:pdfkit #f)
+
+(define-runtime-path that "test11crkt.pdf")
+(make-doc that #t proc)
