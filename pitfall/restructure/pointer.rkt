bfed86d26caab342ff6882356fcb354466be970f
Matthew Butterick <mb@mbtype.com>
2017-06-14 18:05:57 -0700

    touchup

--- pitfall/restructure/pointer.rkt
+++ pitfall/restructure/pointer.rkt
@@ -7,16 +7,16 @@
 
 (for ([(val i) (in-indexed vals)])
   (hash-ref! results i (λ ()
-                    (report* results i)
-                    (let/cc pointer-k
-                      (let ([v (cond
-                                 [(pair? val) (hash-set! pointer-ks (car val) pointer-k) (car val)]
-                                 [else val])])
-                        (define pointer (hash-ref pointer-ks v #f))
-                        (cond
-                          [pointer (hash-remove! pointer-ks v)
-                                   (pointer (format "~a xref" v))]
-                          [else v]))))))
+                         (report* results i)
+                         (let/cc pointer-k
+                           (let ([v (cond
+                                      [(pair? val) (hash-set! pointer-ks (car val) pointer-k) (car val)]
+                                      [else val])])
+                             (define pointer (hash-ref pointer-ks v #f))
+                             (cond
+                               [pointer (hash-remove! pointer-ks v)
+                                        (pointer (format "~a xref" v))]
+                               [else v]))))))
 
 (for/list ([i (in-range (length (hash-keys results)))])
   (hash-ref results i))
