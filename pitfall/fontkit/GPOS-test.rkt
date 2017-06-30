27d06b1a53738057b063be0da1daf4b9c016b298
Matthew Butterick <mb@mbtype.com>
2017-06-30 13:05:16 -0700

    lunch

--- pitfall/fontkit/GPOS-test.rkt
+++ pitfall/fontkit/GPOS-test.rkt
@@ -3,7 +3,6 @@
 
 (define fira-path "../pitfall/test/assets/fira.ttf")
 (define f (openSync fira-path))
-(report 'start-decode)
 (define gpos (send GPOS decode (send f _getTableStream 'GPOS)))
 
 (send (dict-ref gpos 'lookupList) get 2)
