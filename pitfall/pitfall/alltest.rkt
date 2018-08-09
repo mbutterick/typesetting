d7a7d5583730b26dd2444d124f378705a4874643
Matthew Butterick <mb@mbtype.com>
2018-08-08 17:00:58 -0700

    suppress testing

--- pitfall/pitfall/alltest.rkt
+++ pitfall/pitfall/alltest.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (module+ test
-  (require pitfall/test/test0
+  #;(require pitfall/test/test0
            pitfall/test/test1
            pitfall/test/test2
            pitfall/test/test3
