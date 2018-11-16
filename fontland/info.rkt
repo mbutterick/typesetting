763d521056bc1760eb5979630ca2b934350609d0
Matthew Butterick <mb@mbtype.com>
2018-11-15 18:13:03 -0800

    deps

--- fontland/info.rkt
+++ fontland/info.rkt
@@ -1,3 +1,11 @@
 #lang info
 (define collection 'multi)
 (define version "0.0")
+(define deps '("base"
+               "beautiful-racket-lib"
+               "debug"
+               "describe"
+               "draw-lib"
+               "rackunit-lib"
+               "sugar"
+               "xenomorph"))
