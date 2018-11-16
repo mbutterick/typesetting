0c02a0bccc2c48f2f55651624789de8f18edb6b7
Matthew Butterick <mb@mbtype.com>
2018-11-15 16:33:04 -0800

    deps

--- xenomorph/info.rkt
+++ xenomorph/info.rkt
@@ -1,17 +1,9 @@
 #lang info
 (define collection 'multi)
-(define deps '("at-exp-lib"
-               "beautiful-racket-lib"
-               "brag"
-               "describe"
-               "png-image"
-               "srfi-lite-lib"
-               "sugar"
-               "debug"
-               "base" 
-               "draw-lib"
-               "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc"))
 (define version "0.0")
 (define pkg-authors '(mb))
 (define test-omit-paths 'all)
+(define deps '("base"
+               "beautiful-racket-lib"
+               "rackunit-lib"
+               "sugar"))
