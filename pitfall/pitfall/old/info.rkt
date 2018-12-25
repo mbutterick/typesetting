188af54ba015ac4acf40c02a87984545af23d1cc
Matthew Butterick <mb@mbtype.com>
2018-12-24 21:22:01 -0800

    gbye

--- pitfall/pitfall/old/info.rkt
+++
@@ -1,3 +0,0 @@
-#lang info
-(define scribblings '(("scribblings/pitfall.scrbl" ())))
-
--- pitfall/pitfall/old/parser-test.rkt
+++
@@ -1,6 +0,0 @@
-#lang at-exp br
-(require rackunit "parser.rkt" "tokenizer.rkt" brag/support)
-
-(apply-tokenizer-maker make-tokenizer @string-append{(string () here) << /A (B) >>})
-
-#;(parse-to-datum (apply-tokenizer-maker make-tokenizer @string-append{(string () here) << /A (B) >>}))
