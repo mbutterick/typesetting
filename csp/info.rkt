b8dbea9d1305bca415db9fca5ed09be13fff7ff7
Matthew Butterick <mb@mbtype.com>
2018-12-27 14:21:52 -0800

    deps

--- csp/info.rkt
+++ csp/info.rkt
@@ -1,4 +1,10 @@
 #lang info
 (define collection 'multi)
-(define deps '(("base" #:version "6.0") "sugar" "rackunit-lib" "debug" "graph"))
-(define update-implies '("sugar"))
+(define deps '("beautiful-racket-lib"
+               "htdp-lib"
+               "math-lib"
+               ("base" #:version "6.0") "sugar" "rackunit-lib" "debug" "graph"))
+(define update-implies '("sugar"))(define build-deps '("at-exp-lib"
+                     "math-doc"
+                     "racket-doc"
+                     "scribble-lib"))
