97666688e4f4eab1e240ae973fc18ba8978b7b54
Matthew Butterick <mb@mbtype.com>
2019-01-10 12:35:48 -0800

    export more

--- pitfall/pitfall/main.rkt
+++ pitfall/pitfall/main.rkt
@@ -1,7 +1,11 @@
 #lang racket/base
 (require "pdf.rkt"
          "text.rkt"
-         "font.rkt")
+         "font.rkt"
+         "color.rkt"
+         "vector.rkt")
 (provide (all-from-out "pdf.rkt"
                        "text.rkt"
-                       "font.rkt"))
+                       "font.rkt"
+                       "color.rkt"
+                       "vector.rkt"))
