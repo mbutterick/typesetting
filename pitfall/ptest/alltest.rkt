332884f7ba4af85f3f9efc69a29e0f1df0e13b2b
Matthew Butterick <mb@mbtype.com>
2019-05-13 20:55:14 -0500

    2048 em test

--- pitfall/ptest/alltest.rkt
+++ pitfall/ptest/alltest.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(for ([i (in-range 23)])
+(for ([i (in-range 24)])
   (define which (string->symbol (format "ptest/test~a" i)))
   (println which)
   (dynamic-require which #f))
--- pitfall/ptest/test22.rkt
+++ pitfall/ptest/test22.rkt
@@ -1,6 +1,25 @@
 #lang debug racket/base
-(require fontland/test/index
-         fontland/test/glyphs
-         fontland/test/directory
-         fontland/test/subset)
-(time (void))
+(require pitfall/pdftest)
+
+;; subset OTF font
+(define-runtime-path fira "assets/fira.otf")
+(define-runtime-path noto "assets/noto-emoji/NotoEmoji-Regular.ttf")
+
+;; embed otf
+
+(define (proc doc)
+  ;; Use a font with 2048 em (noto)
+  (register-font doc "noto" (path->string noto))
+
+  ;; Set the font, draw some text
+  [font doc "noto"]
+  [font-size doc 40]
+  [text doc "😂😂😂" 100 150])
+
+;; test against non-subsetted font version
+(define-runtime-path this "test22rkt.pdf")
+(make-doc this #f proc)
+
+(define-runtime-path that "test22crkt.pdf")
+(make-doc that #t proc)
+
