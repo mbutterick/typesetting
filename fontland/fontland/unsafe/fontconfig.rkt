92ff1d66defdac85a66c3e9d417550c4c9eaf543
Matthew Butterick <mb@mbtype.com>
2019-06-10 13:29:36 -0700

    internalize

--- fontland/fontland/unsafe/fontconfig.rkt
+++ fontland/fontland/unsafe/fontconfig.rkt
@@ -1,8 +1,8 @@
 #lang racket/base
 
 (require ffi/unsafe
-         ffi-definer-convention
-         
+         ffi/unsafe/define
+         ffi/unsafe/define/conventions
          (for-syntax racket/base
                      racket/string
                      racket/syntax
