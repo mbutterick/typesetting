ae377a189f7a99b6a34b3191d46d7a112280c3a4
Matthew Butterick <mb@mbtype.com>
2020-11-22 21:32:13 -0800

    known tags

--- fontland/fontland/woff2-directory.rkt
+++ fontland/fontland/woff2-directory.rkt
@@ -47,6 +47,7 @@ https://github.com/mbutterick/fontkit/blob/master/src/tables/WOFF2Directory.js
   (check-exn exn:fail? (λ () (decode Base128 (bytes 127 127 127 127 127 128)))) ; overflow
   (check-equal? (decode Base128 (bytes #x3f 128 #x3f)) 63))
 
+(define known-tags '(cmap head hhea hmtx maxp name OS/2 post |cvt | fpgm glyf loca prep |CFF | VORG EBDT EBLC gasp hdmx kern LTSH PCLT VDMX vhea vmtx BASE GDEF GPOS GSUB EBSC JSTF MATH CBDT CBLC COLR CPAL |SVG | sbix acnt avar bdat bloc bsln cvar fdsc feat fmtx fvar gvar hsty just lcar mort morx opbd prop trak Zapf Silf Glat Gloc Feat Sill))
 
 (define woff2-directory-entry
   (x:dict
