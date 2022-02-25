dfd4dc9e74b24e19ddc7d8fa7c8a4b21a0e8526c
Matthew Butterick <mb@mbtype.com>
2022-02-24 16:55:47 -0800

    note to self

--- quad/quad2/atomize.rkt
+++ quad/quad2/atomize.rkt
@@ -1,2 +1,13 @@
 #lang debug racket/base
 
+#|
+
+My idea here is that instead of comparing attrs by using eq? with a hash,
+we can use eq? (not equal?) on an association list
+the idea being that if only update the attrs by consing onto the front,
+then the process is strictly accretive, that is:
+we are never allocating a fresh list to hold existing values
+For instance, the top-level attrs represent a list object
+that will eventually be the tail of the attrs in every atomized quad.
+|#
+
